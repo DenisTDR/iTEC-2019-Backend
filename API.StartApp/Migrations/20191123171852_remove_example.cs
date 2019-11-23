bf804878648395fbfe7991949480468b1637c703
Denis TDR <denis.troncota@ligaac.ro>
2019-11-23 19:20:20 +0200

    removed examples

---
+++ API.StartApp/Migrations/20191123171852_remove_example.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.StartApp.Migrations
+{
+    public partial class remove_example : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Example");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Example",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Age = table.Column<int>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    OrderIndex = table.Column<int>(nullable: false),
+                    Slug = table.Column<string>(nullable: true),
+                    Updated = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Example", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Example_Id",
+                table: "Example",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Example_OrderIndex",
+                table: "Example",
+                column: "OrderIndex");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Example_Slug",
+                table: "Example",
+                column: "Slug");
+        }
+    }
+}
